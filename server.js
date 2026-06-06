855033a7795f55bb34315d4b4cdbf41a80c3ad27
ofissed-jpg <ofissed@gmail.com>
2026-06-06 18:31:50 +0300

    Listen on 0.0.0.0 for Railway

--- server.js
+++ server.js
@@ -526,7 +526,7 @@ app.get('/api/users', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`\n🚀 Сервер запущен на http://localhost:${PORT}`);
   console.log(`📊 База данных: users.db\n`);
 });
